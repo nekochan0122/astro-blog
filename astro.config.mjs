bd0bc410361d64876c412015b21ad5ccdca947a6
NekoChan <47508893+NekoChanTaiwan@users.noreply.github.com>
2022-11-01 14:57:41 -0700

    twemoji

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,45 @@
 import { defineConfig } from 'astro/config'
+
 import tailwind from '@astrojs/tailwind'
 import solidJs from '@astrojs/solid-js'
 import mdx from '@astrojs/mdx'
+import compress from 'astro-compress'
+
 import m2dx from 'astro-m2dx'
 import remarkToc from 'remark-toc'
 
+import rehypeTwemoji from './src/plugins/rehype-twemoji'
+
+// https://astro-m2dx.netlify.app/
 /** @type {import('astro-m2dx').Options} */
 const m2dxOptions = {
   frontmatter: true,
+  exportComponents: true,
   autoImports: false,
+}
+
+/** @type {import('@astrojs/mdx').MdxOptions} */
+const mdxOptions = {
+  remarkPlugins: [[m2dx, m2dxOptions], remarkToc],
+  rehypePlugins: [rehypeTwemoji],
+  recmaPlugins: [],
+  extendPlugins: 'astroDefaults', // remark-gfm, remark-smartypants
+}
+
+const tailwindOptions = {
+  config: {
+    applyBaseStyles: false,
+  },
 }
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs(), mdx()],
+  integrations: [tailwind(tailwindOptions), solidJs(), mdx(mdxOptions), compress()],
   markdown: {
     drafts: true,
     syntaxHighlight: 'shiki',
     shikiConfig: {
       theme: 'one-dark-pro',
     },
-    extendDefaultPlugins: true, // remark-gfm, remark-smartypants
-    remarkPlugins: [[m2dx, m2dxOptions], remarkToc],
-    rehypePlugins: [],
   },
 })
---
+++ src/plugins/rehype-twemoji.js
@@ -0,0 +1,112 @@
+import emojiRegex from 'emoji-regex'
+import replaceToArray from 'string-replace-to-array'
+import twemoji from 'twemoji'
+import { map } from 'unist-util-map'
+
+const defaultTwemojiOptions = {
+  baseUrl: 'https://twemoji.maxcdn.com/v/latest',
+  size: '72x72',
+}
+
+const defaultOptions = {
+  exclude: [],
+  className: 'emoji m-0 inline-block h-8 w-8',
+  twemoji: defaultTwemojiOptions,
+}
+
+const EmojiRegex = emojiRegex()
+const UFE0Fg = /\uFE0F/g
+const U200D = String.fromCharCode(0x200d)
+
+function sizeToExtension(size) {
+  switch (size) {
+    case '72x72':
+      return '.png'
+    case 'svg':
+      return '.svg'
+    default:
+      throw new Error('Unknown size')
+  }
+}
+
+function toCodePoint(emoji) {
+  return twemoji.convert.toCodePoint(emoji.indexOf(U200D) < 0 ? emoji.replace(UFE0Fg, '') : emoji)
+}
+
+function toBaseUrl(codePoint, options) {
+  return `${options.baseUrl}/${options.size}/${codePoint}${sizeToExtension(options.size)}`
+}
+
+function toUrl(emoji, options) {
+  return toBaseUrl(toCodePoint(emoji), options.twemoji)
+}
+
+function makeTransformer(options) {
+  return (tree) => {
+    const mappedChildren = tree.children.map((child) =>
+      map(child, (node) => {
+        if (node.type !== 'text' || !EmojiRegex.test(node.value)) return node
+
+        const children = replaceToArray(node.value, EmojiRegex, (text) => ({
+          emoji: text,
+        })).map((segment) =>
+          typeof segment === 'string'
+            ? {
+                type: 'text',
+                value: segment,
+              }
+            : options.exclude.includes(segment.emoji)
+            ? {
+                type: 'text',
+                value: segment.emoji,
+              }
+            : {
+                type: 'element',
+                tagName: 'img',
+                properties: {
+                  className: [options.className],
+                  draggable: 'false',
+                  alt: segment.emoji,
+                  decoding: 'async',
+                  src: toUrl(segment.emoji, options),
+                },
+                children: [],
+              }
+        )
+
+        const result = {
+          type: 'element',
+          tagName: 'span',
+          children,
+        }
+
+        return result
+      })
+    )
+
+    return {
+      ...tree,
+      children: mappedChildren,
+    }
+  }
+}
+
+function resolveOptions(userOptions) {
+  return userOptions
+    ? {
+        exclude: userOptions.exclude ?? defaultOptions.exclude,
+        className: userOptions.className ?? defaultOptions.className,
+        twemoji: {
+          ...defaultOptions.twemoji,
+          ...userOptions.twemoji,
+        },
+      }
+    : defaultOptions
+}
+
+const rehypeTwemoji = function (userOptions) {
+  const options = resolveOptions(userOptions)
+  return makeTransformer(options)
+}
+
+export default rehypeTwemoji
