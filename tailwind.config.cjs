88f3525914497ca1c088c8c5720da73e17254713
NekoChan <47508893+NekoChanTaiwan@users.noreply.github.com>
2022-11-01 17:11:53 -0700

    add custom fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    fontFamily: {
+      serif: ['Roboto', ...defaultTheme.fontFamily.serif],
+      mono: ['Fira Code', ...defaultTheme.fontFamily.mono],
+    },
     extend: {},
   },
   plugins: [require('@tailwindcss/typography')],
