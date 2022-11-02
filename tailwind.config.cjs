34ec75cf816447528f4d59fd0183c41c568c0cc6
NekoChan <47508893+NekoChanTaiwan@users.noreply.github.com>
2022-11-02 14:59:07 -0700

    add style to home, create Nav

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,7 +9,22 @@ module.exports = {
       serif: ['Inter', ...defaultTheme.fontFamily.serif],
       mono: ['Fira Code', ...defaultTheme.fontFamily.mono],
     },
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          0: '#fff',
+          100: '#fafafa',
+          200: '#eaeaea',
+          300: '#999999',
+          400: '#888888',
+          500: '#666666',
+          600: '#444444',
+          700: '#333333',
+          800: '#222222',
+          900: '#111111',
+        },
+      },
+    },
   },
   plugins: [require('@tailwindcss/typography')],
 }
