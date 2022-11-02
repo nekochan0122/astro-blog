377c44252154ccd3c31871dff3403d5c83835820
NekoChan <47508893+NekoChanTaiwan@users.noreply.github.com>
2022-11-02 11:04:00 -0700

    change font, add dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,10 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: ['class', '[data-theme="dark"]'],
   theme: {
     fontFamily: {
-      serif: ['Roboto', ...defaultTheme.fontFamily.serif],
+      serif: ['Inter', ...defaultTheme.fontFamily.serif],
       mono: ['Fira Code', ...defaultTheme.fontFamily.mono],
     },
     extend: {},
