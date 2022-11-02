ba1bd06208e3a3e199439cc4f7061469ebfa3b34
NekoChan <47508893+NekoChanTaiwan@users.noreply.github.com>
2022-11-02 15:11:10 -0700

    remove children

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,24 +1,17 @@
-import { createSignal, type JSX } from 'solid-js'
+import { createSignal } from 'solid-js'
 import { cx } from '@/lib/utils'
 
-interface Props {
-  children?: JSX.Element
-}
-
-export function Counter(props: Props) {
+export function Counter() {
   const [count, setCount] = createSignal(0)
   const add = () => setCount((i) => i + 1)
   const subtract = () => setCount((i) => i - 1)
 
   return (
-    <>
-      <div class={cx('flex', 'gap-x-4')}>
-        <button onClick={subtract}>-</button>
-        <p>{count()}</p>
-        <button onClick={add}>+</button>
-      </div>
-      <div>{props.children}</div>
-    </>
+    <div class={cx('flex', 'gap-x-4')}>
+      <button onClick={subtract}>-</button>
+      <p>{count()}</p>
+      <button onClick={add}>+</button>
+    </div>
   )
 }
 
