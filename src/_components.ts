5dfbcc6fbf2864b6ef6ef5feb83469a66c187b17
NekoChan <47508893+NekoChanTaiwan@users.noreply.github.com>
2022-11-03 14:53:29 -0700

    create MDX folder, Heading components

--- src/_components.ts
+++ src/_components.ts
@@ -1,8 +1,14 @@
-// @ts-ignore
-import CodeBlock from '@/components/CodeBlock.astro'
+// @ts-nocheck
+import Heading2 from '@/components/MDX/Heading2.astro'
+import Heading3 from '@/components/MDX/Heading3.astro'
+import Heading4 from '@/components/MDX/Heading4.astro'
+import CodeBlock from '@/components/MDX/CodeBlock.astro'
 import { Image } from '@astrojs/image/components'
 
 export const components = {
+  h1: Heading2,
+  h2: Heading3,
+  h3: Heading4,
   pre: CodeBlock,
   img: Image,
 }
