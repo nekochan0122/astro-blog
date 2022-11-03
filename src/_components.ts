f12b0ed56dc2cd1521a85e2c02795ab5f4355ac9
NekoChan <47508893+NekoChanTaiwan@users.noreply.github.com>
2022-11-03 02:59:51 -0700

    bg-transparent

--- src/_components.ts
+++ src/_components.ts
@@ -1,5 +1,8 @@
+// @ts-ignore
+import CodeBlock from '@/components/CodeBlock.astro'
 import { Image } from '@astrojs/image/components'
 
 export const components = {
+  pre: CodeBlock,
   img: Image,
 }
