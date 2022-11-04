faea297da515edaf80ab951421ca347aa6ab84a8
NekoChan <47508893+NekoChanTaiwan@users.noreply.github.com>
2022-11-04 12:08:02 -0700

    getFileName

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
 import { clsx, type ClassArray } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-export function cx(...classes: ClassArray) {
+export function cx(...classes: ClassArray): string {
   return twMerge(clsx(classes))
+}
+
+export function getFileName(path: string): string {
+  return path.match(/([^\/]+)(?=\.\w+$)/)![0]
 }
