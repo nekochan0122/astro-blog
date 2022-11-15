8f21313aaf8cb97950ea8051f1d5f0c82c7ce5fd
NekoChan <47508893+NekoChanTaiwan@users.noreply.github.com>
2022-11-15 10:34:08 -0800

    update counter style

--- src/components/MDX/Counter.tsx
+++ src/components/MDX/Counter.tsx
@@ -2,14 +2,14 @@ import { createSignal } from 'solid-js'
 
 export function Counter() {
   const [count, setCount] = createSignal(0)
+
   const add = () => setCount((i) => i + 1)
-  const subtract = () => setCount((i) => i - 1)
 
   return (
     <div class='flex gap-x-4'>
-      <button onClick={subtract}>-</button>
-      <p>{count()}</p>
-      <button onClick={add}>+</button>
+      <button class='rounded py-2 px-3 transition-colors hover:bg-gray-200 dark:hover:bg-gray-800' onClick={add}>
+        the count is <span class='inline-block min-w-[2ch]'>{count()}</span>
+      </button>
     </div>
   )
 }
