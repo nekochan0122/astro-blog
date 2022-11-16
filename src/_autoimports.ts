503f93376e91ef2e13b6c67f0ca90abbd040e09f
NekoChan <47508893+NekoChanTaiwan@users.noreply.github.com>
2022-11-16 15:19:41 -0800

    create Twemoji component

--- src/_autoimports.ts
+++ src/_autoimports.ts
@@ -1,6 +1,8 @@
 // @ts-nocheck
+import Twemoji from '@/components/MDX/Twemoji.astro'
 import Collapse from '@/components/MDX/Collapse.astro'
 
 export const autoimports = {
+  Twemoji,
   Collapse,
 }
