1e8329e4259cf3e2d2f6345382fb0365fe243e8a
NekoChan <47508893+NekoChanTaiwan@users.noreply.github.com>
2022-11-17 19:49:26 -0800

    create Bookmark

--- src/_autoimports.ts
+++ src/_autoimports.ts
@@ -1,10 +1,12 @@
 // @ts-nocheck
 import Collapse from '@/components/MDX/Collapse.astro'
 import Callout from '@/components/MDX/Callout.astro'
+import Bookmark from '@/components/MDX/Bookmark.astro'
 import Twemoji from '@/components/MDX/Twemoji.astro'
 
 export const autoimports = {
   Collapse,
   Callout,
+  Bookmark,
   Twemoji,
 }
