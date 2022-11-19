65679ceae7ea234fe1ef776b01d1e6b319b1e6e0
NekoChan <47508893+NekoChanTaiwan@users.noreply.github.com>
2022-11-19 15:51:47 -0800

    Frontmatter type

--- src/types/frontmatter.ts
+++ src/types/frontmatter.ts
@@ -1,7 +1,9 @@
 export interface Frontmatter {
   title: string
   description: string
-  date: string
+  publishedAt: string
+  updatedAt?: string
+  author: string
   slug: string
   minutesRead: string
   draft?: boolean
