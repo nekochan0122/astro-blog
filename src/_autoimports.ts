a21dd921b09b541ba40868f57557ded78cac2c38
NekoChan <47508893+NekoChanTaiwan@users.noreply.github.com>
2022-11-18 22:04:54 -0800

    create Spoiler

--- src/_autoimports.ts
+++ src/_autoimports.ts
@@ -2,11 +2,13 @@
 import Collapse from '@/components/MDX/Collapse.astro'
 import Callout from '@/components/MDX/Callout.astro'
 import Bookmark from '@/components/MDX/Bookmark.astro'
+import Spoiler from './components/MDX/Spoiler.astro'
 import Twemoji from '@/components/MDX/Twemoji.astro'
 
 export const autoimports = {
   Collapse,
   Callout,
   Bookmark,
+  Spoiler,
   Twemoji,
 }
---
+++ src/components/MDX/Spoiler.tsx
@@ -0,0 +1,27 @@
+import { createSignal } from 'solid-js'
+import { cx } from '@/lib/utils'
+import type { JSXElement } from 'solid-js'
+
+interface Props {
+  children: JSXElement
+}
+
+export default function Spoiler({ children }: Props) {
+  const [show, setShow] = createSignal(false)
+
+  return (
+    <span
+      class={cx('relative inline-block transition-[filter] duration-300', show() ? 'blur-none' : 'blur-lg')}
+      onClick={() => setShow(!show())}
+    >
+      {children}
+      <span
+        class={cx(
+          'absolute left-0 top-0 h-full w-full backdrop-blur-3xl  backdrop-brightness-125 transition-opacity duration-300',
+          show() && 'opacity-0'
+        )}
+        {...(!show() && { title: 'Click to reveal', role: 'button' })}
+      />
+    </span>
+  )
+}
