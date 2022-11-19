97f67f27b0347aca9c965f9d4dad4dd525ee7719
NekoChan <47508893+NekoChanTaiwan@users.noreply.github.com>
2022-11-18 17:44:49 -0800

    add read-time and date to PostCard

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from 'tailwind-merge'
 export function cx(...classes: ClassArray): string {
   return twMerge(clsx(classes))
 }
+
+export function toISODate(date: string): string {
+  return new Date(date).toISOString()
+}
+
+export function toLocalDate(date: string): string {
+  return new Date(date).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' })
+}
