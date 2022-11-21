e79cfbc6c4ff65aae50ad7fb5e917fd461523249
NekoChan <47508893+NekoChanTaiwan@users.noreply.github.com>
2022-11-21 00:03:53 -0800

    remarkUnwrapImages

--- astro.config.ts
+++ astro.config.ts
@@ -8,11 +8,12 @@ import solidJs from '@astrojs/solid-js'
 import mdx from '@astrojs/mdx'
 import image from '@astrojs/image'
 import compress from 'astro-compress'
-import m2dx from 'astro-m2dx'
-import remarkDebug from './src/plugins/remark-debug'
+import remarkUnwrapImages from 'remark-unwrap-images'
+import remarM2dx from 'astro-m2dx'
 import remarkToc from './src/plugins/remark-toc'
 import remarkRouteSlug from './src/plugins/remark-route-slug'
 import remarkReadingTime from './src/plugins/remark-reading-time'
+import remarkDebug from './src/plugins/remark-debug'
 import rehypeTwemoji from './src/plugins/rehype-twemoji'
 import rehypePrettyCode from 'rehype-pretty-code'
 import type { Options as PrettyCodeOptions } from 'rehype-pretty-code'
@@ -23,7 +24,7 @@ import type { IntegrationOptions } from '@astrojs/image'
 import type { Options as CompressOptions } from 'astro-compress/dist/options'
 
 // https://astro-m2dx.netlify.app/
-const m2dxOptions: M2dxOptions = {
+const remarM2dxOptions: M2dxOptions = {
   frontmatter: true,
   exportComponents: true,
   autoImports: true,
@@ -55,7 +56,14 @@ const prettyCodeOptions: PrettyCodeOptions = {
 }
 
 const mdxOptions: MdxOptions = {
-  remarkPlugins: [[m2dx, m2dxOptions], [remarkToc, remarkTocOptions], remarkRouteSlug, remarkReadingTime, remarkDebug],
+  remarkPlugins: [
+    remarkUnwrapImages,
+    [remarM2dx, remarM2dxOptions],
+    [remarkToc, remarkTocOptions],
+    remarkRouteSlug,
+    remarkReadingTime,
+    remarkDebug,
+  ],
   rehypePlugins: [rehypeTwemoji, [rehypePrettyCode, prettyCodeOptions]],
   extendPlugins: 'astroDefaults', // remark-gfm, remark-smartypants
 }
