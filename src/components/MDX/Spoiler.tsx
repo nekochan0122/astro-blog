72025b13be0c3942e2efb863a7cb9d276105cf3e
NekoChan <47508893+NekoChanTaiwan@users.noreply.github.com>
2022-11-20 16:48:49 -0800

    fix Spoiler in Safari ...

--- src/components/MDX/Spoiler.tsx
+++ src/components/MDX/Spoiler.tsx
@@ -12,8 +12,8 @@ export default function Spoiler({ children }: Props) {
   return (
     <span
       class={cx(
-        'relative inline-block touch-manipulation transition-[filter] duration-300',
-        show() ? 'blur-none' : 'blur-md'
+        'relative inline-block touch-manipulation transition-[filter] duration-300 will-change-[filter]',
+        show() ? 'blur-none' : 'blur'
       )}
       onClick={() => setShow(true)}
       {...(!show() && { title: 'Click to reveal', role: 'button' })}
@@ -21,7 +21,7 @@ export default function Spoiler({ children }: Props) {
       {children}
       <span
         class={cx(
-          'absolute inset-0 bg-black/50 backdrop-blur-3xl transition-opacity duration-300 dark:bg-white/50',
+          'absolute inset-0 bg-gray-600/95 transition-opacity duration-300 dark:bg-gray-300/95',
           show() && 'opacity-0'
         )}
       />
