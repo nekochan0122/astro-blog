3edef8b3c68f9f0ec48fc49956b5db7f9820314e
NekoChan <47508893+NekoChanTaiwan@users.noreply.github.com>
2022-11-21 22:58:02 -0800

    fixed layout shifting by font & install @fontsource/inter

---
+++ postcss.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [
+    require('autoprefixer'),
+    {
+      postcssPlugin: true,
+      Declaration: {
+        'font-display': (node) => {
+          if (node.parent.name === 'font-face' && node.parent.type === 'atrule') node.value = 'optional'
+        },
+      },
+    },
+  ],
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,7 @@ module.exports = {
   darkMode: ['class', '[data-theme="dark"]'],
   theme: {
     fontFamily: {
+      serif: ['Inter', ...fontFamily.sans],
       mono: ['Fira Code', ...fontFamily.mono],
     },
     extend: {
