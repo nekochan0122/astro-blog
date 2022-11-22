592b0cd902b65cf3e7304865f3e8b173316d46fb
NekoChan <47508893+NekoChanTaiwan@users.noreply.github.com>
2022-11-21 22:10:44 -0800

    create Copy button to CodeBlock

--- src/_components.ts
+++ src/_components.ts
@@ -1,9 +1,11 @@
 // @ts-nocheck
+import CodeBlock from '@/components/MDX/CodeBlock.astro'
 import Image from '@/components/MDX/Image.astro'
 import Anchor from '@/components/MDX/Anchor.astro'
 import ListItem from '@/components/MDX/ListItem.astro'
 
 export const components = {
+  pre: CodeBlock,
   img: Image,
   a: Anchor,
   li: ListItem,
