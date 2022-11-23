408fa6193741a6de5362177ab9b48147b5929e15
NekoChan <47508893+NekoChanTaiwan@users.noreply.github.com>
2022-11-22 16:21:21 -0800

    add anchor icon to heading

--- src/_components.ts
+++ src/_components.ts
@@ -1,10 +1,16 @@
 // @ts-nocheck
+import Heading2 from '@/components/MDX/Heading2.astro'
+import Heading3 from '@/components/MDX/Heading3.astro'
+import Heading4 from '@/components/MDX/Heading4.astro'
 import CodeBlock from '@/components/MDX/CodeBlock.astro'
 import Image from '@/components/MDX/Image.astro'
 import Anchor from '@/components/MDX/Anchor.astro'
 import ListItem from '@/components/MDX/ListItem.astro'
 
 export const components = {
+  h2: Heading2,
+  h3: Heading3,
+  h4: Heading4,
   pre: CodeBlock,
   img: Image,
   a: Anchor,
