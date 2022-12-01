a6f0460dd656646f7fe4c38bd5b571edd7acb66e
NekoChan <47508893+NekoChanTaiwan@users.noreply.github.com>
2022-12-01 15:45:30 -0800

    refactor: remove `shortHash` from `utils.ts`

--- src/plugins/remark-toc.ts
+++ src/plugins/remark-toc.ts
@@ -1,6 +1,5 @@
 import { resolve } from 'path'
 import { toc } from 'mdast-util-toc'
-import { shortHash } from '../lib/utils'
 import type { Plugin } from 'unified'
 import type { Options } from 'mdast-util-toc'
 
@@ -40,3 +39,22 @@ const remarkToc: Plugin = (options: Options = { tight: true }) => {
 }
 
 export default remarkToc
+
+function shortHash(data: unknown) {
+  const str = typeof data === 'string' ? data : JSON.stringify(data)
+  const seed = 0
+  let h1 = 0xdeadbeef ^ seed
+  let h2 = 0x41c6ce57 ^ seed
+
+  for (let i = 0, ch: number; i < str.length; i++) {
+    ch = str.charCodeAt(i)
+    h1 = Math.imul(h1 ^ ch, 2654435761)
+    h2 = Math.imul(h2 ^ ch, 1597334677)
+  }
+
+  h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^ Math.imul(h2 ^ (h2 >>> 13), 3266489909)
+  h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^ Math.imul(h1 ^ (h1 >>> 13), 3266489909)
+
+  const h3 = 4294967296 * (2097151 & h2) + (h1 >>> 0)
+  return 't' + h3.toString(32)
+}
